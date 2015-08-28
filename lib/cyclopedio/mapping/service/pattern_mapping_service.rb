4d6fe3e7365be602614b01b63727b03548564504
ericpsherman <ericsherman216@gmail.com>
2015-08-28 14:23:11 +0200

    Remove unused lambda

--- lib/cyclopedio/mapping/service/pattern_mapping_service.rb
+++ lib/cyclopedio/mapping/service/pattern_mapping_service.rb
@@ -52,7 +52,7 @@ module Cyclopedio
             # matched relations computation
             candidates.each do |term|
               counts = []
-              counts.concat(number_of_matched_candidates(parent_candidate_sets,term,candidate_set.full_name,[:genls?]){|t,c| @cyc.genls?(t,c) })
+              counts.concat(number_of_matched_candidates(parent_candidate_sets,term,candidate_set.full_name,[:genls?]))
               counts.concat(number_of_matched_candidates(child_candidate_sets,term,candidate_set.full_name,[:spec?])
               counts.concat(number_of_matched_candidates(instance_candidate_sets,term,candidate_set.full_name,[:type?])
               counts.concat(number_of_matched_candidates(type_candidate_sets,term,"DBPEDIA_TYPE",[:genls?,:spec?,:isa?,:type?]])
